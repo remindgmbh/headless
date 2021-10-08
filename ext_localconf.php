8f635fd1bbb49481f82c1dbe3a60946d7043dd7a
David Mellen <d.mellen@remind.de>
2021-10-08 14:48:28 +0200

    Enable headless.elementBodyResponse

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,3 +2,5 @@
 defined('TYPO3_MODE') || die;
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('@import "EXT:rmnd_headless/Configuration/TSConfig/Page/*.tsconfig"');
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['features']['headless.elementBodyResponse'] = true;
