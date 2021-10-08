7e2992e56f3658fad8b12ac511ed5b1f0daf8470
David Mellen <d.mellen@remind.de>
2021-10-08 14:50:29 +0200

    Add missing labels for felogin
    REHLTYEXT-9

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,3 +4,5 @@ defined('TYPO3_MODE') || die;
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('@import "EXT:rmnd_headless/Configuration/TSConfig/Page/*.tsconfig"');
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['features']['headless.elementBodyResponse'] = true;
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:headless/Resources/Private/Language/locallang.xlf'][] = 'EXT:rmnd_headless/Resources/Private/Language/Overrides/locallang_headless.xlf';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:headless/Resources/Private/Language/de.locallang.xlf'][] = 'EXT:rmnd_headless/Resources/Private/Language/Overrides/de.locallang_headless.xlf';
