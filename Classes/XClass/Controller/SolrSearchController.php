930f2c0b388288426c7db80b71d8e30022cfbc26
David Mellen <d.mellen@remind.de>
2021-10-29 08:46:44 +0200

    Add solr
    REHLTYEXT-8

---
+++ Classes/XClass/Controller/SolrSearchController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Remind\Headless\XClass\Controller;
+
+use ApacheSolrForTypo3\Solr\Controller\SearchController;
+use ApacheSolrForTypo3\Solr\Domain\Search\ResultSet\Result\SearchResult;
+use ApacheSolrForTypo3\Solr\Domain\Search\ResultSet\SearchResultSet;
+use ApacheSolrForTypo3\Solr\System\Solr\SolrUnavailableException;
+use ApacheSolrForTypo3\Solr\ViewHelpers\Document\HighlightResultViewHelper;
+use ApacheSolrForTypo3\Solr\Util;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3Fluid\Fluid\Core\ViewHelper\ViewHelperInvoker;
+
+class SolrSearchController extends SearchController
+{
+
+    /**
+     * @var ViewHelperInvoker
+     */
+    protected $viewHelperInvoker;
+
+    function __construct()
+    {
+        $this->viewHelperInvoker = GeneralUtility::makeInstance(ViewHelperInvoker::class);
+    }
+
+    /**
+     * Results
+     */
+    public function resultsAction()
+    {
+        try {
+            $arguments = (array)$this->request->getArguments();
+            $pageId = $this->typoScriptFrontendController->getRequestedId();
+            $languageId = Util::getLanguageUid();
+            $searchRequest = $this->getSearchRequestBuilder()->buildForSearch($arguments, $pageId, $languageId);
+
+            $searchResultSet = $this->searchService->search($searchRequest);
+
+            $searchResults = $searchResultSet->getSearchResults();
+
+            $mapSearchResult = fn (SearchResult $searchResult) => [
+                'title' => $this->getHighlightedContent($searchResultSet, $searchResult, 'title'),
+                'content' => $this->getHighlightedContent($searchResultSet, $searchResult, 'content'),
+                'url' => json_decode($searchResult->getUrl(), true)
+            ];
+
+            $allResultCount = $searchResultSet->getAllResultCount();
+            $usedResultsPerPage = $searchResultSet->getUsedResultsPerPage();
+            $usedPage = $searchResultSet->getUsedPage();
+
+            $itemsPerPage = (int)ceil($allResultCount / $usedResultsPerPage);
+            $usedQuery = $searchResultSet->getUsedQuery();
+            $query = $usedQuery ? $usedQuery->getOption('query') : null;
+
+            $result = [
+                'documents' => [
+                    'pagination' => [
+                        'current' => $usedPage == 0 && $itemsPerPage > 0  ? 1 : $usedPage,
+                        'numberOfPages' => $itemsPerPage,
+                    ],
+                    'list' => array_map($mapSearchResult, $searchResults->getArrayCopy()),
+                    'count' => $searchResults->getCount()
+                ],
+                'allResultCount' => $allResultCount,
+                'query' => $query
+            ];
+
+            return json_encode(array_merge(['result' => $result], json_decode($this->formAction(), true)));
+        } catch (SolrUnavailableException $e) {
+            $this->handleSolrUnavailable();
+        }
+    }
+
+    /**
+     * Form
+     */
+    public function formAction()
+    {
+        $pluginNamespace = $this->typoScriptConfiguration->getSearchPluginNamespace();
+
+        $queryParams = [
+            'q' => $pluginNamespace . '[q]',
+            'page' => $pluginNamespace . '[page]',
+        ];
+
+        $form = [
+            'pluginNamespace' => $pluginNamespace,
+            'queryParams' => $queryParams
+        ];
+
+        return json_encode(['form' => $form]);
+    }
+
+    protected function getHighlightedContent(SearchResultSet $searchResultSet, SearchResult $searchResult, string $fieldName)
+    {
+        return $this->viewHelperInvoker->invoke(
+            HighlightResultViewHelper::class,
+            ['resultSet' => $searchResultSet, 'document' => $searchResult, 'fieldName' => $fieldName],
+            new \TYPO3\CMS\Fluid\Core\Rendering\RenderingContext(),
+        );
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -8,3 +8,7 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:headless/Resourc
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:headless/Resources/Private/Language/de.locallang.xlf'][] = 'EXT:rmnd_headless/Resources/Private/Language/Overrides/de.locallang_headless.xlf';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:felogin/Resources/Private/Language/locallang.xlf'][] = 'EXT:rmnd_headless/Resources/Private/Language/Overrides/locallang_felogin.xlf';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:felogin/Resources/Private/Language/de.locallang.xlf'][] = 'EXT:rmnd_headless/Resources/Private/Language/Overrides/de.locallang_felogin.xlf';
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][ApacheSolrForTypo3\Solr\Controller\SearchController::class] = [
+    'className' => \Remind\Headless\XClass\Controller\SolrSearchController::class
+];
