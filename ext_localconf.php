56317e31510f9e4da29f8a3421915411214a2d46
David Mellen <d.mellen@remind.de>
2022-06-09 12:18:00 +0200

    Remove non existent pageTs

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,8 +2,6 @@
 
 defined('TYPO3_MODE') || die;
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('@import "EXT:rmnd_headless/Configuration/TSConfig/Page/*.tsconfig"');
-
 $GLOBALS
     ['TYPO3_CONF_VARS']
     ['SYS']['features']
