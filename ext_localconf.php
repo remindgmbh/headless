098d69963d0d53cbb99079e369e3bb94795cb450
David Mellen <d.mellen@remind.de>
2022-08-12 07:27:16 +0200

    Apply configuration files rules and best practices

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,45 +1,50 @@
 <?php
 
-defined('TYPO3_MODE') || die;
+declare(strict_types=1);
 
-$GLOBALS
-    ['TYPO3_CONF_VARS']
-    ['SYS']['features']
-    ['headless.elementBodyResponse'] = true;
+defined('TYPO3') or die();
 
-$GLOBALS
-    ['TYPO3_CONF_VARS']
-    ['SYS']
-    ['locallangXMLOverride']
-    ['EXT:headless/Resources/Private/Language/locallang.xlf']
-    [] = 'EXT:rmnd_headless/Resources/Private/Language/Overrides/locallang_headless.xlf';
+(function () {
+    $GLOBALS
+        ['TYPO3_CONF_VARS']
+        ['SYS']['features']
+        ['headless.elementBodyResponse'] = true;
+    
+    $GLOBALS
+        ['TYPO3_CONF_VARS']
+        ['SYS']
+        ['locallangXMLOverride']
+        ['EXT:headless/Resources/Private/Language/locallang.xlf']
+        [] = 'EXT:rmnd_headless/Resources/Private/Language/Overrides/locallang_headless.xlf';
+    
+    $GLOBALS
+        ['TYPO3_CONF_VARS']
+        ['SYS']
+        ['locallangXMLOverride']
+        ['de']
+        ['EXT:headless/Resources/Private/Language/de.locallang.xlf']
+        [] = 'EXT:rmnd_headless/Resources/Private/Language/Overrides/de.locallang_headless.xlf';
+    
+    $GLOBALS
+        ['TYPO3_CONF_VARS']
+        ['SYS']
+        ['locallangXMLOverride']
+        ['EXT:felogin/Resources/Private/Language/locallang.xlf']
+        [] = 'EXT:rmnd_headless/Resources/Private/Language/Overrides/locallang_felogin.xlf';
+    
+    $GLOBALS
+        ['TYPO3_CONF_VARS']
+        ['SYS']
+        ['locallangXMLOverride']
+        ['de']
+        ['EXT:felogin/Resources/Private/Language/de.locallang.xlf']
+        [] = 'EXT:rmnd_headless/Resources/Private/Language/Overrides/de.locallang_felogin.xlf';
+    
+    $GLOBALS
+        ['TYPO3_CONF_VARS']
+        ['SYS']
+        ['Objects']
+        [ApacheSolrForTypo3\Solr\Controller\SearchController::class] = 
+            ['className' => \Remind\Typo3Headless\XClass\Controller\SolrSearchController::class];
+})();
 
-$GLOBALS
-    ['TYPO3_CONF_VARS']
-    ['SYS']
-    ['locallangXMLOverride']
-    ['de']
-    ['EXT:headless/Resources/Private/Language/de.locallang.xlf']
-    [] = 'EXT:rmnd_headless/Resources/Private/Language/Overrides/de.locallang_headless.xlf';
-
-$GLOBALS
-    ['TYPO3_CONF_VARS']
-    ['SYS']
-    ['locallangXMLOverride']
-    ['EXT:felogin/Resources/Private/Language/locallang.xlf']
-    [] = 'EXT:rmnd_headless/Resources/Private/Language/Overrides/locallang_felogin.xlf';
-
-$GLOBALS
-    ['TYPO3_CONF_VARS']
-    ['SYS']
-    ['locallangXMLOverride']
-    ['de']
-    ['EXT:felogin/Resources/Private/Language/de.locallang.xlf']
-    [] = 'EXT:rmnd_headless/Resources/Private/Language/Overrides/de.locallang_felogin.xlf';
-
-$GLOBALS
-    ['TYPO3_CONF_VARS']
-    ['SYS']
-    ['Objects']
-    [ApacheSolrForTypo3\Solr\Controller\SearchController::class] = 
-        ['className' => \Remind\Typo3Headless\XClass\Controller\SolrSearchController::class];
