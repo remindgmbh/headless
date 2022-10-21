edb7d4f5b1093da8f70358024d12379713b0af64
David Mellen <d.mellen@remind.de>
2022-10-21 08:28:52 +0200

    Update typoscript include title

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -7,5 +7,5 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 ExtensionManagementUtility::addStaticFile(
     'rmnd_headless',
     'Configuration/TypoScript',
-    'Remind Headless'
+    'REMIND - Headless'
 );
