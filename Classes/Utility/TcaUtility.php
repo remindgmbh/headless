587a3c002d5d9a6a14cdd5bc21425ac2c6166a83
David Mellen <d.mellen@remind.de>
2023-11-13 09:00:00 +0100

    Allow multiple aspect ratios for each breakpoint

--- Classes/Utility/TcaUtility.php
+++ Classes/Utility/TcaUtility.php
@@ -7,20 +7,35 @@ namespace Remind\Headless\Utility;
 class TcaUtility
 {
     /**
-     * @param array $variants array with breakpoint names as key and aspect-ratio as array value containing value and title, e.g. ['lg' => [8 / 5, '8:5'], 'xxl' => [16 / 9, '16:9']]
+     * @param array $variants array with breakpoint names as key and aspect-ratios as value
+     * the value can contain multple aspect ratios consisting of value and title
+     * e.g. [
+     *          'lg' => [
+     *              ['value' => 8 / 5, 'title' => '8:5 (default)'],
+     *              ['value' => 2 / 1, 'title' => '2:1 (alternative layout)'],
+     *          ],
+     *          'xxl' => [
+     *              ['value' => 16 / 9, 'title' => '16:9']
+     *          ]
+     *      ]
      */
     public static function getCropVariants(array $variants): array
     {
         return array_reduce(array_keys($variants), function (array $result, string $breakpoint) use ($variants) {
-            [$value, $title] = $variants[$breakpoint];
+            $aspectRatios = $variants[$breakpoint];
             $result[$breakpoint] = [
                 'title' => $breakpoint,
-                'allowedAspectRatios' => [
-                    $breakpoint => [
-                        'value' => $value,
-                        'title' => $title,
-                    ],
-                ],
+                'allowedAspectRatios' => array_reduce(array_keys($aspectRatios), function (
+                    array $result,
+                    string $key
+                ) use (
+                    $aspectRatios,
+                    $breakpoint,
+                ) {
+                    $aspectRatio = $aspectRatios[$key];
+                    $result[$breakpoint . '_' . $aspectRatio['value']] = $aspectRatio;
+                    return $result;
+                }, []),
             ];
             return $result;
         }, []);
