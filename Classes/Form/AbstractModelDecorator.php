b007f540b5cab884e9970e611a70a8eff791145e
David Mellen <d.mellen@remind.de>
2023-11-17 09:29:57 +0100

    Add abstractModelDecorator

---
+++ Classes/Form/AbstractModelDecorator.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Remind\Headless\Form;
+
+use FriendsOfTYPO3\Headless\Form\Decorator\AbstractFormDefinitionDecorator;
+use Psr\Http\Message\ServerRequestInterface;
+
+abstract class AbstractModelDecorator extends AbstractFormDefinitionDecorator
+{
+    protected string $actionName = '';
+    protected string $controllerName = '';
+    protected string $valueName = '';
+
+    protected function overrideDefinition(array $decorated, array $definition, int $currentPage): array
+    {
+        $request = $this->getRequest();
+
+        /** @var \TYPO3\CMS\Core\Routing\PageArguments $pageArguments */
+        $pageArguments = $request->getAttribute('routing');
+
+        $arguments = $pageArguments->getArguments();
+
+        $controllerArguments = $arguments[$this->controllerName] ?? null;
+
+        if ($controllerArguments) {
+            $uid = (int) $controllerArguments[$this->valueName] ?? null;
+            $action = $controllerArguments['action'] ?? null;
+
+            if ($action === $this->actionName && $uid) {
+                $decorated['elements'][] = [
+                    'type' => 'Hidden',
+                    'defaultValue' => $uid,
+                    'name' => $this->controllerName . '[' . $this->valueName . ']',
+                ];
+            }
+        }
+
+        return $decorated;
+    }
+
+    private function getRequest(): ServerRequestInterface
+    {
+        return $GLOBALS['TYPO3_REQUEST'];
+    }
+}
