3531a02c9d6d702b99afe19b443d17f8ac33b063
KonstantCode <k.friedrich@remind.de>
2024-06-12 10:09:45 +0000

    feat: expand DataEventListener to account for non-standard File attributes

--- Classes/Event/Listener/EnrichFileDataEventListener.php
+++ Classes/Event/Listener/EnrichFileDataEventListener.php
@@ -11,7 +11,28 @@ class EnrichFileDataEventListener
     public function __invoke(EnrichFileDataEvent $event): void
     {
         $originalFile = $event->getOriginal();
+        $processedFile = $event->getProcessed();
         $properties = $event->getProperties();
+        $processingConfiguration = $event->getProcessingConfiguration();
+
+        // class ProcessingConfiguration always inserts arrays for these defaultFields, so the following checks shouldnt be necessary, but check anyway
+        $defaultFieldsByType = $processingConfiguration->defaultFieldsByType ?? [];
+        $defaultImageFields = array_merge($defaultFieldsByType, $processingConfiguration->defaultImageFields ?? []);
+        $defaultVideoFields = array_merge($defaultFieldsByType, $processingConfiguration->defaultVideoFields ?? []);
+
+        $defaultFields = match ($properties['type']) {
+            'image' => $defaultImageFields,
+            'video' => $defaultVideoFields,
+            default => $defaultFieldsByType,
+        };
+
+        // processedFile has to be used here instead of originalFile, otherwise pre processed attributes (like extension = svg) get readded
+        foreach ($defaultFields as $field) {
+            if ($processedFile->hasProperty($field) && !isset($properties[$field])) {
+                $properties[$field] = $processedFile->getProperty($field);
+            }
+        }
+
         $properties['lazyLoading'] = (bool) $originalFile->getProperty('tx_headless_lazy_loading') ?? true;
         $event->setProperties($properties);
     }
