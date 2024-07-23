af27e1f48c6be178d4e7fdcca07f48a581b97e28
David Mellen <d.mellen@remind.de>
2024-07-23 13:57:34 +0200

    feat: add links to form checkbox

--- Classes/Form/FormDefinitionDecorator.php
+++ Classes/Form/FormDefinitionDecorator.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Remind\Headless\Form;
 
 use FriendsOfTYPO3\Headless\Form\Decorator\AbstractFormDefinitionDecorator;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
 class FormDefinitionDecorator extends AbstractFormDefinitionDecorator
 {
@@ -14,27 +16,52 @@ class FormDefinitionDecorator extends AbstractFormDefinitionDecorator
         1347992400,
         1347992453,
     ];
+
+    private ContentObjectRenderer $cObj;
+    public function __construct(array $formStatus = [])
+    {
+        parent::__construct($formStatus);
+        $this->cObj = GeneralUtility::makeInstance(ContentObjectRenderer::class);
+    }
+
     protected function overrideDefinition(array $decorated, array $definition, int $currentPage): array
     {
         foreach ($decorated['elements'] as &$element) {
-            $notEmptyValidators = array_filter($element['validators'] ?? [], function (array $validator) {
-                return $validator['identifier'] === 'NotEmpty';
-            });
-            if ($notEmptyValidators) {
-                foreach ($element['properties']['validationErrorMessages'] ?? [] as $validationErrorMessageKey => $validationErrorMessage) {
-                    if (in_array($validationErrorMessage['code'], self::NOT_EMPTY_ERROR_CODES)) {
-                        foreach (array_keys($notEmptyValidators) as $validatorKey) {
-                            $element['validators'][$validatorKey]['customErrorMessage'] = $validationErrorMessage['customMessage'];
-                        }
-                        unset($element['properties']['validationErrorMessages'][$validationErrorMessageKey]);
-                    }
-                }
-                if (empty($element['properties']['validationErrorMessages'])) {
-                    unset($element['properties']['validationErrorMessages']);
-                }
-            }
-            unset($element['properties']['fluidAdditionalAttributes']);
+            $this->setNotEmptyValidationErrorMessages($element);
+            $this->setCheckboxLinks($element);
         }
         return $decorated;
+    }
+
+    private function setNotEmptyValidationErrorMessages(array &$element): void
+    {
+        $notEmptyValidators = array_filter($element['validators'] ?? [], function (array $validator) {
+            return $validator['identifier'] === 'NotEmpty';
+        });
+        if ($notEmptyValidators) {
+            foreach ($element['properties']['validationErrorMessages'] ?? [] as $validationErrorMessageKey => $validationErrorMessage) {
+                if (in_array($validationErrorMessage['code'], self::NOT_EMPTY_ERROR_CODES)) {
+                    foreach (array_keys($notEmptyValidators) as $validatorKey) {
+                        $element['validators'][$validatorKey]['customErrorMessage'] = $validationErrorMessage['customMessage'];
+                    }
+                    unset($element['properties']['validationErrorMessages'][$validationErrorMessageKey]);
+                }
+            }
+            if (empty($element['properties']['validationErrorMessages'])) {
+                unset($element['properties']['validationErrorMessages']);
+            }
+        }
+        unset($element['properties']['fluidAdditionalAttributes']);
+    }
+
+    private function setCheckboxLinks(array &$element): void
+    {
+        if ($element['type'] === 'Checkbox' && isset($element['properties']['links'])) {
+            foreach ($element['properties']['links'] as $pageUid => $label) {
+                $link = $this->cObj->createLink($label, ['parameter' => $pageUid])->getHtml();
+                $element['label'] = sprintf($element['label'], $link);
+            }
+            unset($element['properties']['links']);
+        }
     }
 }
