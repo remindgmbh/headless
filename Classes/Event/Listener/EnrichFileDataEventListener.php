32f5916512112047c36f69ea290c4c45c002490f
David Mellen <d.mellen@remind.de>
2024-08-27 13:01:11 +0200

    feat: allow non-standard file attributes

--- Classes/Event/Listener/EnrichFileDataEventListener.php
+++ Classes/Event/Listener/EnrichFileDataEventListener.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Remind\Headless\Event\Listener;
 
 use FriendsOfTYPO3\Headless\Event\EnrichFileDataEvent;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class EnrichFileDataEventListener
 {
@@ -12,7 +13,31 @@ class EnrichFileDataEventListener
     {
         $originalFile = $event->getOriginal();
         $properties = $event->getProperties();
-        $properties['lazyLoading'] = (bool) $originalFile->getProperty('tx_headless_lazy_loading');
+        $processingConfiguration = $event->getProcessingConfiguration();
+
+        $defaultFields = $processingConfiguration->defaultFieldsByType ?? [];
+        $imageFields = array_merge($defaultFields, $processingConfiguration->defaultImageFields ?? []);
+        $videoFields = array_merge($defaultFields, $processingConfiguration->defaultVideoFields ?? []);
+
+        $fields = match ($properties['type']) {
+            'image' => $imageFields,
+            'video' => $videoFields,
+            default => $defaultFields,
+        };
+
+        foreach ($fields as $field) {
+            $as = $field;
+            if (str_contains($field, ' as ')) {
+                [$field, $as] = GeneralUtility::trimExplode(' as ', $field, true);
+            }
+            if (
+                $originalFile->hasProperty($field) &&
+                !array_key_exists($as, $properties)
+            ) {
+                $properties[$as] = $originalFile->getProperty($field);
+            }
+        }
+
         $event->setProperties($properties);
     }
 }
